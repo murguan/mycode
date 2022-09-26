deaf6aee613e618a1e035d0955142fe0a8251925
git@github.com:murguan/mycode.git <git@github.com:murguan/mycode.git>
2022-09-26 20:23:00 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+## List of animals
+"""Alta3 Research | RZFeeser
+   List - making selections from complex lists"""
+
+def main():
+
+    # create a list called list1
+    list1 = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+    
+    # display list1
+    print(list1)
+
+main()
