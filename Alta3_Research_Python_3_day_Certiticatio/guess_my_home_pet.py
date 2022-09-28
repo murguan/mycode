95bad2fa00d6a2d9b11af6d925e88b203cb5fe1f
git@github.com:murguan/mycode.git <git@github.com:murguan/mycode.git>
2022-09-28 18:57:08 +0000

    my cert lab test

--- Alta3_Research_Python_3_day_Certiticatio/guess_my_home_pet.py
+++ Alta3_Research_Python_3_day_Certiticatio/guess_my_home_pet.py
@@ -7,14 +7,19 @@ def main():
     round = 0                                    # integer initiated to zero
     while True:                                  # sets infinite loop condition
         round = round + 1                        # Increase the round counter
-        print("Answer in lower case only")
         print("What kind of pet do you think I have? ")
         answer = input("you guess---> ")         # string of answer collected
         if answer == 'dog':                      # Logic to check if correct answer
             print('WOW! You are correct!')
             break                                # Break statement exit the while loop 
+        elif answer == 'DOG':                    # All CAPS string 
+            print('WOW! You are correct!')       # Logic check
+            break                                # Break statement exit the while loop
+        elif answer == 'Dog':                    # Captial first letter of correct answer
+            print('WOW! You are correct!')       # Logic check
+            break                                # Break statement exit the while loop
         elif round == 3:                         # Last chance to guess
-            print('Sorry, the answer is dog.')   
+            print('Sorry, the answer is dog.')   # If entered wrong answer 
             break                                # Break statemnet, exit loop
         else:                                    # If wrong answer is not equal to 3, continue 
             print('Not quite, try again!')        
